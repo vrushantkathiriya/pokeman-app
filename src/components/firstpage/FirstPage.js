c232e8c2568bcebcf36414fcd03fd6441e1db8eb
vrushantkathiriya <vrushantkathiriya@gmail.com>
2023-05-04 18:08:49 +0530

    moves moduls

--- src/components/firstpage/FirstPage.js
+++ src/components/firstpage/FirstPage.js
@@ -5,16 +5,24 @@ import pokeball1 from '../images/pokeball 1.png'
 import pokeball2 from '../images/pokeball 2.png'
 import pokeball3 from '../images/pokeball 3.png'
 import moves from '../images/move 1.png'
-import ampharos from '../images/ampharos 1.png'
+import ampharos from '../images/ampharos 1.png'  
 import Blissey from '../images/Blissey 1.png'
-import garchomp1 from '../images/garchomp 1.png'
+import garchomp1 from '../images/garchomp 1.png' 
 import gardevoir from '../images/gardevoir 1.png'
 import gengar1 from '../images/gengar 1.png'
 import politoad from '../images/politoad 1.png'
 import moonicon from '../images/moon.png'
 import sunicon from '../images/sun.png'
+// import  Moves from './components/firstpage/Moves';    
+import { useNavigate } from "react-router-dom";
+
 const FirstPage = () => {
     const [darkmode, setDarkmode] = useState(false)
+    let history = useNavigate();
+
+    const ClickMoves = () =>{
+        history("/Moves");
+    }
     return (
         <>
             <div>
@@ -29,7 +37,7 @@ const FirstPage = () => {
                         <img src={pokeball2} className='pokeball2' />
                         <img src={pokeball3} className='pokeball3' />
                     </div>
-                    <div className={`moves ${darkmode ? "moves_darkmode_active" : ""}`} onClick={()=>console.log('moves')}><p>Moves</p>
+                    <div className={`moves ${darkmode ? "moves_darkmode_active" : ""}`} onClick={ClickMoves}><p>Moves</p>
                         <img src={moves} className='moveimg' /></div>
                     <div className={`team ${darkmode ? "team_darkmode_active" : ""}`} onClick={()=>console.log('team')}><p>Your Team</p>
                         <img src={politoad} className='politoad' />
